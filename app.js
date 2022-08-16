84bab246b3bfc3514a506893d3784c83e445761d
Petryk Oleksandr <sachapos50@gmail.com>
2022-08-16 22:55:10 +0300

    save

---
+++ app.js
@@ -0,0 +1,44 @@
+const appConfig = {
+  data() {
+    return {
+      title: "Notes list",
+      inputPlaceholder: "Note",
+      inputValue: "",
+      notes: ['buy milk'],
+    }
+  },
+  methods: {
+    inputHandler(e) {
+      this.inputValue = e.target.value;
+    },
+    addHandler() {
+      if (this.inputValue !== "") {
+        this.notes.push(this.inputValue);
+        this.inputValue = "";
+      }
+    },
+    deleteNote(i) {
+      this.notes.splice(i, 1);
+    },
+    upperCase(item) {
+      return item.toUpperCase();
+    },
+  },
+  computed: {
+    doubleCountComputed() {
+      console.log('computes double count')
+      return this.notes.length * 2;
+    },
+  },
+  watch: {
+    inputValue(value) {
+      if (value.length > 10) {
+        this.inputValue = "";
+      }
+    },
+  },
+};
+
+const App = Vue.createApp(appConfig);
+
+App.mount('#app');
