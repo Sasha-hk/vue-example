e9233e013d4244ee8d4f57f8c95314c1020e0ef7
Petryk Oleksandr <sachapos50@gmail.com>
2022-09-15 12:39:39 +0300

    feat: for safety

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
 import Container from './UI/Container.vue';
+import Button from './UI/Button.vue';
 import Section from './UI/Section.vue';
 import Input from './UI/Input.vue';
-import Button from './UI/Button.vue';
 
 export default [
   Container,
+  Button,
   Section,
   Input,
-  Button,
 ];
